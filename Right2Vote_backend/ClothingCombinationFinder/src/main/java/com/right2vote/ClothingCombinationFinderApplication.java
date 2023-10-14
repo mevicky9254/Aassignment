699ef3bfe97f569de3208b6b6105ac727c014f20
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-10-14 20:37:04 +0530

    Initial commit

---
+++ Right2Vote_backend/ClothingCombinationFinder/src/main/java/com/right2vote/ClothingCombinationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.right2vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingCombinationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingCombinationFinderApplication.class, args);
+	}
+
+}
