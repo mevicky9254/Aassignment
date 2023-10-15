10508763498ce2f3147c8b88d119ab9ce2cdcd6f
mevicky9254 <vikkichoudhary6528@gmail.com>
2023-10-15 20:37:13 +0530

    added the loader incase of delay in fetching the response

--- Right2Vote_frontend/index.js
+++ Right2Vote_frontend/index.js
@@ -1,33 +1,47 @@
-
 const calculateButton = document.getElementById('calculate');
 const budgetInput = document.getElementById('budget');
 const resultList = document.getElementById('result-list');
 const resultContainer = document.querySelector('.result_container');
 const totalLength = document.getElementById('total-length');
+const loader = document.getElementById('loader'); 
 
 calculateButton.addEventListener('click', () => {
-    resultList.innerHTML=null;
+    resultList.innerHTML = null;
     const budget = parseFloat(budgetInput.value);
-    console.log("budget ="+budget);
+    console.log("budget =" + budget);
 
-    if (isNaN(budget)) {
+    if (isNaN(budget) || budget < 230) {
+        alert("Insufficient budget");
         resultList.innerHTML = 'Please enter a valid budget.';
         totalLength.innerHTML = '';
     } else {
-        const apiUrl = 'https://backendservice-6vll.onrender.com/combination?budget=' + budget; 
+        const url = 'https://backendservice-6vll.onrender.com/combination?budget=' + budget;
 
-        fetch(apiUrl)
+        
+        loader.style.display = 'block';
+
+        fetch(url)
             .then(response => response.json())
             .then(data => {
-                //console.log(data);
+                
+                loader.style.display = 'none';
+
                 data.forEach(item => {
-                    console.log(item)
+                    console.log(item);
                     resultList.innerHTML += `<p>${item}</p>`;
                 });
                 totalLength.innerHTML = `Total combinations: ${data.length}`;
+            })
+            .catch(error => {
+                loader.style.display = 'none';
+                alert("Taking longer time than usual")
+                console.error('An error occurred:', error);
             });
-            resultContainer.style.display = 'block';
+
+             setTimeout(() => {
+               loader.style.display = 'none';
+             }, 3000); 
+
+        resultContainer.style.display = 'block';
     }
 });
-
-
